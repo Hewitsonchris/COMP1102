e1a1b9a8e75d6b96bd34d701294463c8bd002dbf
Hewitsonchris <Hewitsonchris@gmail.com>
2014-01-23 16:40:12 +1030

    Create ComputeTax.java
    If I wish to account for invalid marital status or income type (e.g. defacto and -$ respectively) I must alter the relative else statements.  What is the best format?  replace else with if's?

---
+++ ComputeTax.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Tax1
+{
+   public static void main(String[] args)
+   {
+      final double RATE_LOW = 0.10;     
+      final double RATE_MIDDLE = 0.15;
+      final double RATE_HIGH = 0.25;
+
+      String   status;
+      double income;
+      double tax;
+ 
+      DecimalFormat fmt = new DecimalFormat("$0.00");
+      Scanner scan = new Scanner(System.in); 
+ 
+      System.out.println("Enter marital status (S or M): ");
+      status = scan.next();        
+      System.out.print("Enter income = ");
+      income = scan.nextDouble(); 
+      
+      if (status.equals("s") || status.equals("S"))
+      {  
+         if (income > 0 && income <=8000)
+            tax = RATE_LOW * income;
+         else if (income <= 32000)
+            tax = (RATE_MIDDLE * income)  + 800;               
+         else
+            tax = (RATE_HIGH * income) + 4400;
+      }
+      else
+      {  
+         if (income > 0 && income <=16000)
+            tax = RATE_LOW * income;
+         else if ( income <= 64000 )
+            tax = (RATE_MIDDLE * income) + 1600;
+         else
+            tax = RATE_HIGH * income + 8800;
+      }
+
+      System.out.println("Tax = " + fmt.format(tax));
+   }
+}
